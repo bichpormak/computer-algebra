4deb879d044622d00aa40e9078de7dfa7bc5d32f
AnenkovIvan <vanyanenkov@gmail.com>
2024-11-09 14:15:11 +0300

    fix

--- Anenkov_Ivan/natural_number.cpp
+++ Anenkov_Ivan/natural_number.cpp
@@ -1,9 +1,10 @@
-#include "number.h" 
+#include "natural_number.h"   
 
-std::unique_ptr<Number> NaturalNumber::subtract_with_multiply_digit(NaturalNumber& other, int64_t digit) const { 
+std::unique_ptr<Number> NaturalNumber::subtract_with_multiply_digit(NaturalNumber& other, int64_t digit) const {
 
-    NaturalNumber *subtract_with_multiply_digit = new NaturalNumber(this->subtract(other.multiply_by_digit(digit))); 
-    
-    return std::unique_ptr<Number>(subtract_with_multiply_digit); 
-} 
+    std::unique_ptr<Number> multiplied = other.multiply_by_digit(digit);
 
+    std::unique_ptr<Number> result = this->subtract(*multiplied);
+
+    return result;
+}
