96a14c9c9ced5f62fe381f0f848590d2fd054347
VladislavZayt <vladislav-zaytsev-90@mail.ru>
2024-11-09 19:05:31 +0300

    Add n1 n4

--- src/model/natural_number.cpp
+++ src/model/natural_number.cpp
@@ -1,8 +1,30 @@
 #include "natural_number.h"
 
 std::unique_ptr<Number> NaturalNumber::add(Number &other) const {
+    auto num1 = this->get_digits_of_number();// сохранение цифр первого числа 
+    auto num2 = other.get_digits_of_number();
+    if (this->compare(static_cast<NaturalNumber &>(other)) == 1){ // поиск большего числа 
+        auto tmp = num2;
+        num2 = num1;
+        num1 = tmp;
+    };
+    std::reverse(num1.begin(), num1.end()); // разворот большего числа 
+    std::reverse(num2.begin(), num2.end());
+    for (size_t k = 0; k < num2.size(); ++k){ 
 
-    return nullptr;
+        num1[k] += num2[k]; // поразрядовое сложение цифр
+        if (num1[k] >= 10 && k + 1 < num1.size()){ // случай, когда при сложении разрядов получилось число, которое больше 10. При этом это не максимальный разраяд
+            num1[k + 1] += 1;
+            num1[k] -= 10;
+        }
+        else if(num1[k] >= 10 && k + 1 == num1.size()){ // случай, когда при сложении разрядов получилось число, которое больше 10. При этом это максимальный разраяд
+            num1.emplace_back(1);
+            num1[k] -= 10;
+        }
+    }
+    std::reverse(num1.begin(), num1.end()); // разворот числа 
+    return std::make_unique<Number>((gather_digits_into_number(num1)));
+    
 }
 
 std::unique_ptr<Number> NaturalNumber::subtract(Number &other) const {
@@ -11,8 +33,21 @@ std::unique_ptr<Number> NaturalNumber::subtract(Number &other) const {
 }
 
 std::unique_ptr<Number> NaturalNumber::multiply(Number &other) const {
+    auto num1 = this->get_digits_of_number();// сохранение цифр первого числа 
+    auto num2 = other.get_digits_of_number();
+    std::unique_ptr<NaturalNumber> result = std::make_unique<NaturalNumber>(0);
 
-    return nullptr;
+    std::reverse(num1.begin(), num1.end());
+    std::reverse(num2.begin(), num2.end());
+    std::unique_ptr<NaturalNumber> tmp = std::make_unique<NaturalNumber>(gather_digits_into_number(num1));
+    
+    for (size_t k = 0; k < num2.size(); ++k){ // проход по цифрам множетеля
+        tmp->multiply_by_digit(num2[k]);
+        result->add(tmp->multiply_by_ten_in_power(k));
+        
+    }
+
+    return std::make_unique<Number>(gather_digits_into_number(result->get_digits_of_number()));
 }
 
 std::unique_ptr<Number> NaturalNumber::divide(Number &other) const {
@@ -31,8 +66,29 @@ std::unique_ptr<Number> NaturalNumber::change_sign() const {
 }
 
 uint8_t NaturalNumber::compare(NaturalNumber &other) const {
+    auto num1 = this->get_digits_of_number(); // сохранение цифр первого числа 
+    auto num2 = other.get_digits_of_number();
+    int result = 0; 
+    if (num1.size() > num2.size()){ // сравнение, если количество разрядов в первом числе больше количества разрядов во втором
+        result = 2;
+    }
+    else if (num1.size() < num2.size()){ // сравнение, если количество разрядов в первом числе меньше количества разрядов во втором
+        result = 1;
+    }
+    else{ // поразрядовое сравнение, если количетсво разрядов совпадает
+        for (size_t i = 0; i < num1.size(); ++i){
+            if(num1[i] > num2[i]){
+                result = 2;
+                break;
+            }
+            else if (num1[i] < num2[i]){
+                result = 1;
+                break;
+            }
+        }
+    }
 
-    return 0;
+    return result;
 }
 
 bool NaturalNumber::is_zero() const {
