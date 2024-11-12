b9f42c98265c97fe6b2c7616abda8550fa6dc243
Max1m05 <144446477+Max1m05@users.noreply.github.com>
2024-11-12 15:32:53 +0300

    add Z1, Z4

--- src/model/integer_numbers.cpp
+++ src/model/integer_numbers.cpp
@@ -42,9 +42,11 @@ std::unique_ptr<IntegerNumber> IntegerNumber::multiply(NaturalNumber& other) con
     return nullptr;
 }
 
-std::unique_ptr<IntegerNumber> IntegerNumber::get_absolute_value() const {
-
-    return nullptr;
+std::unique_ptr<IntegerNumber> IntegerNumber::get_absolute_value(Number& other) const {
+    if(other->get_sign() == SignNumber::NEGATIVE){
+        return change_sign(other);
+    }
+    return other;
 }
 
 std::unique_ptr<IntegerNumber> IntegerNumber::change_sign() const {
@@ -52,9 +54,12 @@ std::unique_ptr<IntegerNumber> IntegerNumber::change_sign() const {
     return nullptr;
 }
 
-std::unique_ptr<IntegerNumber> IntegerNumber::converting_natural_to_integer() const {
-
-    return nullptr;
+std::unique_ptr<IntegerNumber> IntegerNumber::converting_natural_to_integer(Number& other) const {
+    auto* other_natural = dynamic_cast<NaturalNumber*>(&other);
+    if (!other_natural) {
+        throw std::invalid_argument("Incompatible number type for converting natural to integer");
+    }
+    return std::make_unique<IntegerNumber>(other_natural->get_value());
 }
 
 std::unique_ptr<IntegerNumber> IntegerNumber::converting_positive_integer_to_natural() const {
