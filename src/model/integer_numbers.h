5d2f9c6a09be35b41c683d7bb2f8852b18f8e5f5
Max1m05 <144446477+Max1m05@users.noreply.github.com>
2024-11-12 15:34:41 +0300

    Update integer_numbers.h

--- src/model/integer_numbers.h
+++ src/model/integer_numbers.h
@@ -12,10 +12,10 @@ public:
     std::unique_ptr<IntegerNumber> add(NaturalNumber& other) const;
     std::unique_ptr<IntegerNumber> subtract(NaturalNumber& other) const;
     std::unique_ptr<IntegerNumber> multiply(NaturalNumber& other) const;
-    std::unique_ptr<IntegerNumber> get_absolute_value() const;
+    std::unique_ptr<IntegerNumber> get_absolute_value(Number& other) const;
     std::unique_ptr<IntegerNumber> change_sign() const;
     int8_t is_number_positive() const;
-    std::unique_ptr<IntegerNumber> converting_natural_to_integer() const;
+    std::unique_ptr<IntegerNumber> converting_natural_to_integer(Number& other) const;
     std::unique_ptr<IntegerNumber> converting_positive_integer_to_natural() const;
     std::unique_ptr<IntegerNumber> calculating_quotient() const;
     std::unique_ptr<IntegerNumber> calculating_remainder_after_division() const;
