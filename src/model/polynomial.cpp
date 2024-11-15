1498ffd1aac7a31b4876d5ea2d5abcdc982c001d
Max1m05 <chermax005@gmail.com>
2024-11-15 16:19:00 +0300

    add P8, P10

--- src/model/polynomial.cpp
+++ src/model/polynomial.cpp
@@ -1,1 +1,18 @@
 #include "polynomial.h"
+#include <memory>
+
+std::unique_ptr<Polynomial> Polynomial::multiply(const Polynomial& other) const{
+    auto res = std::make_unique<Polynomial>();
+    for(const auto& iter : coefficients_){
+        auto polinomial = other.multiply_by_rational(*iter.second);
+        auto temp = polinomial->multiply_by_x_power(*iter.first);
+        res->add(*temp);
+    }
+    return res;
+}
+
+std::unique_ptr<Polynomial> Polynomial::remainder(const Polynomial& divisor) const{
+    auto quotient = this->divide(divisor);
+    auto temp = quotient->multiply(divisor);
+    return std::make_unique<Polynomial>(this->subtract(*temp));
+}
