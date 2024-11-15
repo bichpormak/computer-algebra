faa805aedcd8bb21d16bb12289b5337a08be5c96
zemerovas <zemerovasv@mail.ru>
2024-11-16 02:13:07 +0300

    P5,P6

--- src/model/polynomial.cpp
+++ src/model/polynomial.cpp
@@ -1,37 +1,10 @@
 
-    std::unique_ptr<Polynomial> add(const Polynomial& other) const {
-        auto result = std::make_unique<Polynomial>();
+const RationalNumber& leading_coefficient() const{
+    return coefficients_[std::prev(coefficients_.end())];
+}
 
-        for (const auto& [key, value] : coefficients_) {
-            result->coefficients_[key] = std::make_unique<RationalNumber>(*value);
-        }
-
-        for (const auto& [key, value] : other.coefficients_) {
-            if (result->coefficients_.find(key) != result->coefficients_.end()) {
-                result->coefficients_[key].add(*value);
-            } else {
-                result->coefficients_[key] = std::make_unique<RationalNumber>(*value);
-            }
-        }
-        return result; 
-    }
-
-    std::unique_ptr<Polynomial> subtract(const Polynomial& other) const{
-        auto result = std::make_unique<Polynomial>();
-
-        for (const auto& [key, value] : coefficients_) {
-            result->coefficients_[key] = std::make_unique<RationalNumber>(*value);
-        }
-
-        for (const auto& [key, value] : other.coefficients_) {
-            if (result->coefficients_.find(key) != result->coefficients_.end()) {
-                result->coefficients_[key].subtract(*value);
-            } else {
-                result->coefficients_[key] = std::make_unique<RationalNumber>(-*value);
-            }
-        }
-        return result; 
-
-    }
+int degree() const{
+    return std::prev(coefficients_.end())
+}
 
 
